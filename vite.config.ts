9d1c678b070537e17dea162fe45770ebd3283aff
teukusem <syamteuku4@gmail.com>
2025-01-27 15:16:56 +0700

    feat: integrate PWA support with asset generation and update index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
 import dotenv from 'dotenv';
+import { VitePWA } from 'vite-plugin-pwa';
 
 dotenv.config();
 
 export default defineConfig({
   root: 'src',
+  publicDir: '../public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
@@ -31,6 +33,45 @@ export default defineConfig({
     react(),
     environment('all', { prefix: 'CANISTER_' }),
     environment('all', { prefix: 'DFX_' }),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Vite PWA Project',
+        short_name: 'Vite PWA Project',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+        background_color:'#f0e7db',
+        display:"standalone",
+        scope:'/',
+        start_url:"/",
+        orientation:'portrait'
+      
+      }, 
+    })
   ],
   test: {
     environment: 'jsdom',
